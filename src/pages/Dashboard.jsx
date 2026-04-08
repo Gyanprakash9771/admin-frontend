a1af3d4b2567d65af74ea353297418f70da4e58f
Gyanprakash9771 <gyanprakash9771@gmail.com>
2026-04-08 17:25:18 +0530

    updated api url

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -115,7 +115,7 @@ export default function Dashboard() {
               >
                 <div className="flex items-center gap-3">
                   <img
-                    src={`http://localhost:5000/uploads/${c.image}`}
+                    src={`https://edutest-backend-0r41.onrender.com/uploads/${c.image}`}
                     className="w-10 h-10 rounded"
                   />
                   <span>{c.title}</span>
